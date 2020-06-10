2d7aca58b2084e225897e6d8bf41af9923769914
joeygufreda <joey@gufreda.com>
2020-06-10 14:18:30 -0400

    Removed gibberish and added time and date stamp

--- GenerateNonsense/index.js
+++ GenerateNonsense/index.js
@@ -1,6 +1,7 @@
 module.exports = async function (context, req) {
     context.log('Generating Nonsense...');
+    let now = new Date ();
     context.res = {
-        body: "Bibble wibble wobble"
+        body: "The date and time now is: " + now.toISOString() + "T" + now.toTimeString()
     };
 };
