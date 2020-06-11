f8713d942411cbf478e18d1b124196d7417a21b4
joeygufreda <joey@gufreda.com>
2020-06-11 11:07:23 -0400

    Removed redundant time string.

--- GenerateNonsense/index.js
+++ GenerateNonsense/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('Generating Nonsense...');
     let now = new Date ();
     context.res = {
-        body: "The date and time now is: " + now.toISOString() + "T" + now.toTimeString()
+        body: "The date and time now is: " + now.toISOString()
     };
 };
